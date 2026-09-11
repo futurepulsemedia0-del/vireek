fa72490171bdcb1d971fee26caceb9f9f9ce45d0
ALI <futurepulse.media0@gmail.com>
2026-09-11 13:16:28 +0330

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -115,6 +115,7 @@ const FOOTER_COLUMNS = [
       { label: 'Careers', href: '/careers' },
       { label: 'Press', href: '/press' },
       { label: 'Partners', href: '/partners' },
+      { label: 'Affiliate Program', href: '/affiliate' },{ label: 'Affiliate Program', href: '/affiliate' },
       { label: 'Contact', href: '/contact' },
     ],
   },
