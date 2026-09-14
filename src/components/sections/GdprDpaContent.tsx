ebfd5fcf050957971ce6890d6648915909ae52d7
ALI <futurepulse.media0@gmail.com>
2026-09-14 11:55:27 +0330

    Update GdprDpaContent.tsx

--- src/components/sections/GdprDpaContent.tsx
+++ src/components/sections/GdprDpaContent.tsx
@@ -103,7 +103,6 @@ export function GdprDpaContent() {
               <nav className="flex flex-col gap-1 border-l border-border">
                 {SECTIONS.map((s) => (
                   <a
-                  
                     key={s.id}
                     href={`#${s.id}`}
                     className={`-ml-px border-l-2 py-1.5 pl-4 text-sm transition-colors ${
