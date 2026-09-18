d4e7fb0bd290cba1fa23c09de4fa51f7fc03aa48
ALI <futurepulse.media0@gmail.com>
2026-09-18 13:21:57 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,7 @@ const BenchmarksPage = lazy(() => import('@/pages/BenchmarksPage').then((m) => (
 const QuoteAcceptPage = lazy(() => import('@/pages/QuoteAcceptPage').then((m) => ({ default: m.QuoteAcceptPage })));
 const DispatchBoardPage = lazy(() => import('@/pages/DispatchBoardPage').then((m) => ({ default: m.DispatchBoardPage })));
 const InsuranceClaimsPage = lazy(() => import('@/pages/InsuranceClaimsPage').then((m) => ({ default: m.InsuranceClaimsPage })));
+const LaborMarketplacePage = lazy(() => import('@/pages/LaborMarketplacePage').then((m) => ({ default: m.LaborMarketplacePage })));
 const CommercialContractsPage = lazy(() => import('@/pages/CommercialContractsPage').then((m) => ({ default: m.CommercialContractsPage })));
 const ReviewsPage = lazy(() => import('@/pages/ReviewsPage').then((m) => ({ default: m.ReviewsPage })));
 const MembershipsPage = lazy(() => import('@/pages/MembershipsPage').then((m) => ({ default: m.MembershipsPage })));
@@ -545,6 +546,14 @@ function App() {
           element={
             <ProtectedRoute>
               <InsuranceClaimsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/labor-marketplace"
+          element={
+            <ProtectedRoute>
+              <LaborMarketplacePage />
             </ProtectedRoute>
           }
         />
