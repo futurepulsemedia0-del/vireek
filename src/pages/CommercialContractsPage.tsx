71025d884bc9a01ef91cf85e5dbee47ca03be0fe
ALI <futurepulse.media0@gmail.com>
2026-09-19 09:06:49 +0330

    Update CommercialContractsPage.tsx

--- src/pages/CommercialContractsPage.tsx
+++ src/pages/CommercialContractsPage.tsx
@@ -892,7 +892,6 @@ export function CommercialContractsPage() {
                         </div>
                       )}
                     </motion.div>
-                  )
                 )}
               </div>
             )}
