3c00c5f07924a32d8ccbebd3bdf98a43a925f5e5
ALI <futurepulse.media0@gmail.com>
2026-09-25 18:56:36 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -103,6 +103,7 @@ const CommitmentGraphPage = lazy(() => import('@/pages/CommitmentGraphPage').the
 const CompanyReflexesPage = lazy(() => import('@/pages/CompanyReflexesPage').then((m) => ({ default: m.CompanyReflexesPage })));
 const BusinessEvolutionRoadmapPage = lazy(() => import('@/pages/BusinessEvolutionRoadmapPage').then((m) => ({ default: m.BusinessEvolutionRoadmapPage })));
 const BusinessConstitutionPage = lazy(() => import('@/pages/BusinessConstitutionPage').then((m) => ({ default: m.BusinessConstitutionPage })));
+const SelfEvolvingModelPage = lazy(() => import('@/pages/SelfEvolvingModelPage').then((m) => ({ default: m.SelfEvolvingModelPage })));
 const ValueAtRiskPage = lazy(() => import('@/pages/ValueAtRiskPage').then((m) => ({ default: m.ValueAtRiskPage })));
 const BottleneckMarketMakerPage = lazy(() => import('@/pages/BottleneckMarketMakerPage').then((m) => ({ default: m.BottleneckMarketMakerPage })));
 const UncertaintyMapPage = lazy(() => import('@/pages/UncertaintyMapPage').then((m) => ({ default: m.UncertaintyMapPage })));
@@ -554,6 +555,14 @@ function App() {
           element={
             <ProtectedRoute>
               <RegretConsolePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/operating-model"
+          element={
+            <ProtectedRoute>
+              <SelfEvolvingModelPage />
             </ProtectedRoute>
           }
         />
