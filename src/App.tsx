6f200049eaf94676e4fc6c4ae54cea15031155d3
ALI <futurepulse.media0@gmail.com>
2026-09-26 15:56:02 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ const CustomerIntelligencePage = lazy(() => import('@/pages/CustomerIntelligence
 const TradePlaybooksPage = lazy(() => import('@/pages/TradePlaybooksPage').then((m) => ({ default: m.TradePlaybooksPage })));
 const PlaybooksPage = lazy(() => import('@/pages/PlaybooksPage').then((m) => ({ default: m.PlaybooksPage })));
 const AgentGovernancePage = lazy(() => import('@/pages/AgentGovernancePage').then((m) => ({ default: m.AgentGovernancePage })));
+const AgentCompanyPage = lazy(() => import('@/pages/AgentCompanyPage').then((m) => ({ default: m.AgentCompanyPage })));
 const AutonomyBudgetPage = lazy(() => import('@/pages/AutonomyBudgetPage').then((m) => ({ default: m.AutonomyBudgetPage })));
 const ExecutionReliabilityPage = lazy(() => import('@/pages/ExecutionReliabilityPage').then((m) => ({ default: m.ExecutionReliabilityPage })));
 const OutboundCampaignsPage = lazy(() => import('@/pages/OutboundCampaignsPage').then((m) => ({ default: m.OutboundCampaignsPage })));
@@ -985,6 +986,14 @@ function App() {
           element={
             <ProtectedRoute>
               <AgentGovernancePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/agent-company"
+          element={
+            <ProtectedRoute>
+              <AgentCompanyPage />
             </ProtectedRoute>
           }
         />
