01bfe16a31dd9af34df4b01fe8242873e9fd366b
ALI <futurepulse.media0@gmail.com>
2026-09-26 17:41:51 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -122,6 +122,7 @@ const GovernanceCenterPage = lazy(() => import('@/pages/GovernanceCenterPage').t
 const SelfEvolvingModelPage = lazy(() => import('@/pages/SelfEvolvingModelPage').then((m) => ({ default: m.SelfEvolvingModelPage })));
 const ValueAtRiskPage = lazy(() => import('@/pages/ValueAtRiskPage').then((m) => ({ default: m.ValueAtRiskPage })));
 const RevenueAutopsyPage = lazy(() => import('@/pages/RevenueAutopsyPage').then((m) => ({ default: m.RevenueAutopsyPage })));
+const LtvAutopilotPage = lazy(() => import('@/pages/LtvAutopilotPage').then((m) => ({ default: m.LtvAutopilotPage })));
 const BottleneckMarketMakerPage = lazy(() => import('@/pages/BottleneckMarketMakerPage').then((m) => ({ default: m.BottleneckMarketMakerPage })));
 const UncertaintyMapPage = lazy(() => import('@/pages/UncertaintyMapPage').then((m) => ({ default: m.UncertaintyMapPage })));
 const EventBusPage = lazy(() => import('@/pages/EventBusPage').then((m) => ({ default: m.EventBusPage })));
@@ -837,6 +838,14 @@ function App() {
   element={
     <ProtectedRoute>
       <RevenueAutopsyPage />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/dashboard/ltv-autopilot"
+  element={
+    <ProtectedRoute>
+      <LtvAutopilotPage />
     </ProtectedRoute>
   }
 />
